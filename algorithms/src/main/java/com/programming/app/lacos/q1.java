0c1e8c8ffaff0f46a75741101da8571eaaa49779
RobertMiguel <robertresende9@gmail.com>
2024-02-24 19:32:04 -0300

    feat: remove q1

--- algorithms/src/main/java/com/programming/app/lacos/q1.java
+++ algorithms/src/main/java/com/programming/app/lacos/q1.java
@@ -19,32 +19,8 @@ public class q1 {
       System.out.println("Digite o valor do número 4° :");
       valor04 = input.nextInt();
 
-      if(valor01 > valor02) {
-        int valorMaior = valor01;
-        int valorMenor = valor02;
-        valor01 = valorMenor;
-        valor02 = valorMaior;
-      }
-      if (valor03 > valor04) {
-        int valorMaior = valor03;
-        int valorMenor = valor04;
-        valor03 = valorMenor;
-        valor04 = valorMaior;
-      }
-      if (valor01 > valor03) {
-        int valorMaior = valor01;
-        int valorMenor = valor03;
-        valor01 = valorMenor;
-        valor03 = valorMaior;
-      }
-      if (valor02 > valor04) {
-        int valorMaior = valor02;
-        int valorMenor = valor04;
-        valor02 = valorMenor;
-        valor04 = valorMaior;
-      }
 
-      System.out.println("Ordem crescente : " + valor01 + " " + valor02 + " " + valor03 + " " + valor04);
+
 
     }
 
