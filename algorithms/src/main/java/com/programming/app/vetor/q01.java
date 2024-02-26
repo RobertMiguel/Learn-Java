f311063601f4f68e74436e977b793f7be9336662
RobertMiguel <robertresende9@gmail.com>
2024-02-26 15:58:32 -0300

    feat: q01 cap array

--- algorithms/src/main/java/com/programming/app/vetor/q01.java
+++ algorithms/src/main/java/com/programming/app/vetor/q01.java
@@ -1,7 +1,35 @@
 package com.programming.app.vetor;
 
+import java.util.Scanner;
+
 public class q01 {
   public static void main(String[] args) {
     
+    Scanner input = new Scanner(System.in);
+
+    int i, j;
+    int contador;
+    int numeros[] = new int[9];
+
+    for (i = 0; i < 9; i++) {
+      System.out.println("Digite um número :");
+      numeros[i] = input.nextInt();
+    }
+
+    for (i = 0; i < 9; i++) {
+      contador = 0 ; 
+      for (j = 1; j < numeros[i]; j++) {
+        if (numeros[i] % j == 0) {
+          contador++;
+        }
+      }
+      if (contador < 2) {
+        System.out.println("Número : " + numeros[i] + " é primo"); 
+        System.out.println("Na posição : " + i);
+      }
+    }
+
+
+    input.close();
   }
 }
