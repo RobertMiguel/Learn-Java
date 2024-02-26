da82a5a3e52605b491596d38594224b3344604f8
RobertMiguel <robertresende9@gmail.com>
2024-02-26 18:02:55 -0300

    feat: q3 array

---
+++ algorithms/src/main/java/com/programming/app/vetor/q03.java
@@ -0,0 +1,39 @@
+package com.programming.app.vetor;
+
+import java.util.Scanner;
+
+public class q03 {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    int i, j = 0;
+    int numero;
+    int vetor01[] = new int[10];
+    int vetor02[] = new int[10];
+    int intercalacao[] = new int[20];
+
+    for (i = 0; i < 10; i++) {
+      System.out.println("Digite um número para o vetor 1 :");
+      vetor01[i] = input.nextInt();
+      intercalacao[j] = vetor01[i];
+      j++;
+    }
+
+    j = 0;
+
+    for (i = 0; i < 10; i++) {
+      System.out.println("Digite um número para o vetor 2 :");
+      vetor02[i] = input.nextInt();
+      intercalacao[j + 1] = vetor02[i];
+      j += 2;
+    }
+
+    for (int k = 0; k < intercalacao.length; k++) {
+      System.out.println(intercalacao[k]);
+    }
+
+    input.close();
+  }
+}
+
