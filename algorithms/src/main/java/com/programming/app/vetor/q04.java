95d0ec692bfbe44a7eb019db6e8d34a03983d5b8
RobertMiguel <robertresende9@gmail.com>
2024-02-27 11:12:51 -0300

    feat: q04 array

---
+++ algorithms/src/main/java/com/programming/app/vetor/q04.java
@@ -0,0 +1,49 @@
+package com.programming.app.vetor;
+
+import java.util.Scanner;
+
+public class q04 {
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+
+    int i, j;
+    int numeros[] = new int[8];
+    int positivos[] = new int[8];
+    int negativo[] = new int[8];
+    int indicePositivo = 1, indiceNegativo = 1;
+
+    for (i = 0; i < 8; i++) {
+      System.out.println("Digite um número :");
+      numeros[i] = input.nextInt();
+
+      if (numeros[i] > 0 && indicePositivo < 8) {
+          positivos[indicePositivo] = numeros[i];
+          indicePositivo++;
+      } else if (numeros[i] < 0 && indiceNegativo < 8) {
+        negativo[indiceNegativo] = numeros[i];
+        indiceNegativo++;
+      }
+    }
+
+    if (indicePositivo == 0) {
+      System.out.println("Array positivo vazio!");
+    } else {
+      System.out.println("Array negativo :");
+      for (j = 0; j < indicePositivo; j++) {
+        System.out.println(positivos[j]);
+      }
+    }
+    if (indiceNegativo == 0) {
+      System.out.println("Array negativo vazio!");
+    } else {
+      System.out.println("Array negativo :");
+      for(j = 0; j < indiceNegativo; j++) {
+        System.out.println(negativo[j]);
+      }
+    }
+
+    input.close();
+
+  }
+}
