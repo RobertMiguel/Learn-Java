3d97262c1ddd2f05acaabeea409b33368cb6cd87
RobertMiguel <robertresende9@gmail.com>
2024-03-03 12:02:42 -0300

    feat: questão nojentaaa do meu odio

--- algorithms/src/main/java/com/programming/app/arrays/q08.java
+++ algorithms/src/main/java/com/programming/app/arrays/q08.java
@@ -12,20 +12,40 @@ public class q08 {
 
     int vetor01[] = new int[5];
     int vetor02[] = new int[5];
+    int vetor03[] = new int[10];
+    int j = 0;
 
     System.out.println("Preecha o vetor 1 :");
 
     for (int i = 0; i < vetor01.length; i++) {
       vetor01[i] = input.nextInt();
+      vetor03[j] = vetor01[i];
+      j++;
     }
 
-    System.out.println("Preecha o vetor 1 :");
+    System.out.println("Preecha o vetor 2 :");
 
     for (int i = 0; i < vetor02.length; i++) {
       vetor02[i] = input.nextInt();
+      vetor03[j] = vetor02[i];
+      j++;
     }
 
+    for (int i = 0; i < vetor03.length; i++) {
+      for (int k = 0; k < vetor03.length - k - 1; k++) {
+        if (vetor03[k] > vetor03[k + 1]) {
+          int temp = vetor03[k];
+          vetor03[k] = vetor03[k + 1];
+          vetor03[k + 1] = temp;
+        }
+      }
+    }
 
+    System.out.println("Ordem crescente :");
+
+    for (int i = 0; i < vetor03.length; i++) {
+      System.out.println(vetor03[i]);
+    }
 
     input.close();
 
